f07f336175b86f9df9cdf5a1b99582613096f682
田世洋 <tianshiyang@honganrobots.com>
2023-01-06 01:01:32 +0800

    first commit

---
+++ packages/vant-use/src/useRelation/useParent.ts
@@ -0,0 +1,42 @@
+import {
+  ref,
+  inject,
+  computed,
+  onUnmounted,
+  InjectionKey,
+  getCurrentInstance,
+  ComponentPublicInstance,
+  ComponentInternalInstance,
+} from 'vue';
+
+type ParentProvide<T> = T & {
+  link(child: ComponentInternalInstance): void;
+  unlink(child: ComponentInternalInstance): void;
+  children: ComponentPublicInstance[];
+  internalChildren: ComponentInternalInstance[];
+};
+
+export function useParent<T>(key: InjectionKey<ParentProvide<T>>) {
+  const parent = inject(key, null);
+
+  if (parent) {
+    const instance = getCurrentInstance()!;
+    const { link, unlink, internalChildren } = parent;
+
+    // 向父组件的子组件们中添加当前子组件
+    link(instance);
+    onUnmounted(() => unlink(instance));
+
+    const index = computed(() => internalChildren.indexOf(instance));
+
+    return {
+      parent,
+      index,
+    };
+  }
+
+  return {
+    parent: null,
+    index: ref(-1),
+  };
+}
